b35d4fe4b4471c02ae400166d2923213ed61671b
pieceofcakey <choi_927@naver.com>
2022-06-22 15:55:40 +0900

    주석 다시 해제

--- routes/kakaoRouter.js
+++ routes/kakaoRouter.js
@@ -8,29 +8,27 @@ const { JWT_SECRET_KEY } = process.env;
 
 router.post('/auth/kakao', async (req, res) => {
   console.log(req.body);
-  const { email, nickname } = req.body;
+  const { email, nickname, userImage } = req.body;
 
   const existsUsers = await User.findOne({ email });
   console.log(existsUsers);
-  // if (existsUsers) {
-  // 이미 해당 이메일이 DB에 있는 경우 DB에 new User로 새로 테이블을 만들어주지 않고 토큰만 보내준다.
-  // res.send({
-  //   result: true,
-  //   token: jwt.sign({ email: existsUsers.email }, JWT_SECRET_KEY),
-  // });
-  // return;
-  // }
-  // else {
-  const user = await User.create({
-    nickname,
-    email,
-  });
-  // return
-  res.send({
-    result: true,
-    token: jwt.sign({ email: user.email }, JWT_SECRET_KEY),
-  });
-  // }
+  if (existsUsers) {
+    // 이미 해당 이메일이 DB에 있는 경우 DB에 new User로 새로 테이블을 만들어주지 않고 토큰만 보내준다.
+    return res.send({
+      result: true,
+      token: jwt.sign({ email: existsUsers.email }, JWT_SECRET_KEY),
+    });
+  } else {
+    const user = await User.create({
+      nickname,
+      email,
+      userImage,
+    });
+    return res.send({
+      result: true,
+      token: jwt.sign({ email: user.email }, JWT_SECRET_KEY),
+    });
+  }
   // await user.save();
 });
 
