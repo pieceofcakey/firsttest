a3a75f8b61f22929ba8fd273642c1f2677d85b30
pieceofcakey <choi_927@naver.com>
2022-06-23 00:47:10 +0900

    kakao

--- routes/kakaoRouter.js
+++ routes/kakaoRouter.js
@@ -10,15 +10,19 @@ router.post('/auth/kakao', async (req, res) => {
   console.log(req.body);
   const { email, nickname, userImage } = req.body;
 
-  const existsUsers = await User.findOne({ email });
+  const existsUsers = await User.findOne({ email: email });
   console.log(existsUsers);
   if (existsUsers) {
     // 이미 해당 이메일이 DB에 있는 경우 DB에 new User로 새로 테이블을 만들어주지 않고 토큰만 보내준다.
     return res.send({
       result: true,
-      token: jwt.sign({ email: existsUsers.email }, JWT_SECRET_KEY, {
-        expiresIn: '6h',
-      }),
+      token: jwt.sign(
+        { userId: existsUsers._id.toHexString() },
+        JWT_SECRET_KEY,
+        {
+          expiresIn: '6h',
+        }
+      ),
     });
   } else {
     const user = await User.create({
@@ -28,7 +32,7 @@ router.post('/auth/kakao', async (req, res) => {
     });
     return res.send({
       result: true,
-      token: jwt.sign({ email: user.email }, JWT_SECRET_KEY, {
+      token: jwt.sign({ userId: user._id.toHexString() }, JWT_SECRET_KEY, {
         expiresIn: '6h',
       }),
     });
